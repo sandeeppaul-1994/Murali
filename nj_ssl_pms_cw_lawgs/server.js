21faa10d3f7b13449d4e7239d8d0fcbd893a243a
muralindia <muralindia@gmail.com>
2019-05-04 08:51:24 +0530

    Update server.js
    update details about log group

--- nj_ssl_pms_cw_lawgs/server.js
+++ nj_ssl_pms_cw_lawgs/server.js
@@ -10,10 +10,13 @@ lawgs.config({
         region: 'us-east-1'
 	}
 });
-
+// multiple folder path was used here for testing.
 app.get('/abc/a2b/v1/apple/accounts', function (req, res) {
   res.send('accounts')
+	// fgnodejsloggroup should be created in CloudWatch log group earlier. 
+	// lawgs.getOrCreate was observed as not creating the log group
   var logger  = lawgs.getOrCreate('fgnodejsloggroup'); /* LogGroup */
+	// abca2bdevqa is the log stream name used to store logs
   logger.log('abca2bdevqa', { team: 'apple accounts', weight: 7 });
 })
 app.get('/abc/a2b/v1/apple/contacts', function (req, res) {
